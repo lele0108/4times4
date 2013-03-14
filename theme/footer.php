7b03e8c5dedc9db6436f6d695d0659f3afff12b8
Jimmy Liu <sicong.liu98@gmail.com>
2013-03-14 01:49:31 -0700

    Code Cleanup + Spinning images!

--- theme/footer.php
+++ theme/footer.php
@@ -1,7 +1,7 @@
 <div class="footer">
    <div class="span7 offset4">
         <hr>
-        <p>Created by Jimmy Liu. Inspired by Medium and Svbtle. Powered by AnchorCMS.</p>
+        <p>Created by Jimmy Liu. Inspired by Medium and svbtle. Powered by AnchorCMS and Bootstrap.</p>
         </div>
         </div>
         
@@ -10,6 +10,5 @@
 
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script>prettyPrint();</script>
   </body>
 </html>
